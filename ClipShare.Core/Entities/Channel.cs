24e5e35b66735ebdc0dea5bc485cab2754f7a2c2
JClerve <joesonclerve9@gmail.com>
2025-06-20 22:59:53 +0530

    Some Entities and Relations are added

---
+++ ClipShare.Core/Entities/Channel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipShare.Core.Entities
+{
+    public class Channel : BaseEntity
+    {
+        public String Name { get; set; }
+        public String About { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int AppUserId { get; set; }
+
+        //Navigation
+        public AppUser AppUser { get; set; }
+
+        public ICollection<Video> Videos { get; set; }
+    }
+}
